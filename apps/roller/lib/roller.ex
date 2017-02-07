ab2baec3bdc6ab3408fe3ea96831c390f60496ff
Alan Gardner <urfolomeus@gmail.com>
2017-02-07 22:01:52 +0000

    Roll multiple d6

--- apps/roller/lib/roller.ex
+++ apps/roller/lib/roller.ex
@@ -4,15 +4,17 @@ defmodule Roller do
   """
 
   @doc """
-  Rolls the given dice. Returns an integer.
+  Parses the given roll string, rolls the result and returns it.
   """
   @spec roll(String.t()) :: integer
-  def roll("d" <> die) do
-    roll_dice(String.to_integer(die))
+  def roll("d" <> die), do: roll("1d" <> die)
+  def roll(die_string) do
+    roll_dice(String.split(die_string, "d"))
   end
 
-  @spec roll_dice(integer) :: integer
-  defp roll_dice(die) do
-    :rand.uniform(die)
+  defp roll_dice([amount_string, die_string]) do
+    amount = String.to_integer(amount_string)
+    roll = :rand.uniform(String.to_integer(die_string))
+    amount * roll
   end
 end
--- apps/roller/test/roller_test.exs
+++ apps/roller/test/roller_test.exs
@@ -7,15 +7,39 @@ defmodule RollerTest do
 
   import Roller
 
-  test "rolling a d6 returns an integer between 1 and 6" do
+  test "rolling a d6 with no specified number returns an integer between 1 and 6" do
     result = roll("d6")
 
     # Run this a few times each run so that we're more likely to catch bugs
     # in the pseudo random number generation
-    for _ <- 1..10 do
+    for _ <- 1..10_000 do
       assert is_integer(result)
       assert result >= 1
       assert result <= 6
+    end
+  end
+
+  test "rolling 1d6 returns an integer between 1 and 6" do
+    result = roll("1d6")
+
+    # Run this a few times each run so that we're more likely to catch bugs
+    # in the pseudo random number generation
+    for _ <- 1..10_000 do
+      assert is_integer(result)
+      assert result >= 1
+      assert result <= 6
+    end
+  end
+
+  test "rolling 2d6 returns an integer between 2 and 12" do
+    result = roll("2d6")
+
+    # Run this a few times each run so that we're more likely to catch bugs
+    # in the pseudo random number generation
+    for _ <- 1..10_000 do
+      assert is_integer(result)
+      assert result >= 2
+      assert result <= 12
     end
   end
 end
