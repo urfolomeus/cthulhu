d5288e479bc09783d9485c0bb6a6fc5b09c7699d
Alan Gardner <urfolomeus@gmail.com>
2017-02-08 11:35:46 +0000

    Handling more complex rolls

--- apps/roller/lib/roller.ex
+++ apps/roller/lib/roller.ex
@@ -7,15 +7,38 @@ defmodule Roller do
   Parses the given roll string, rolls the result and returns it.
   """
   @spec roll(String.t(), function) :: integer
-  def roll(die_string, rand \\ :rand)
-  def roll("d" <> die, rand), do: roll("1d" <> die, rand)
-  def roll(die_string, rand) do
-    roll_dice(String.split(die_string, "d"), rand)
+  def roll(die_string, rand \\ :rand) do
+    regex = ~r/^(?<amount>\d*)d(?<die>\d+)(?<mod>\+|-*)(?<value>\d*)$/
+    captures = Regex.named_captures(regex, die_string)
+
+    do_roll(captures, rand)
   end
 
-  defp roll_dice([amount_string, die_string], rand) do
-    amount = String.to_integer(amount_string)
-    roll = rand.uniform(String.to_integer(die_string))
+  defp do_roll(nil, _rand), do: raise ArgumentError, message: "Invalid die string"
+  defp do_roll(%{"amount" => "", "die" => d, "mod" => m, "value" => v}, rand) do
+    do_roll(%{"amount" => "1", "die" => d, "mod" => m, "value" => v}, rand)
+  end
+  defp do_roll(%{"amount" => a, "die" => d, "mod" => ""}, rand) do
+    roll_dice(a, d, rand)
+  end
+  defp do_roll(%{"amount" => a, "die" => d, "mod" => m, "value" => ""}, rand) do
+    do_roll(%{"amount" => a, "die" => d, "mod" => m, "value" => "0"}, rand)
+  end
+  defp do_roll(%{"amount" => a, "die" => d, "mod" => "+", "value" => v}, rand) do
+    value  = String.to_integer(v)
+    roll_dice(a, d, rand) + value
+  end
+  defp do_roll(%{"amount" => a, "die" => d, "mod" => "-", "value" => v}, rand) do
+    value  = String.to_integer(v)
+    roll_dice(a, d, rand) - value
+  end
+
+  defp roll_dice(a, d, rand) do
+    amount = String.to_integer(a)
+    die    = String.to_integer(d)
+
+    roll = rand.uniform(die)
+
     amount * roll
   end
 end
--- apps/roller/test/roller_test.exs
+++ apps/roller/test/roller_test.exs
@@ -7,27 +7,121 @@ defmodule RollerTest do
 
   import Roller
 
-  test "rolling a d6 with no specified number returns the randomly rolled value" do
+  setup do
     stub_rand = Stubr.stub!([uniform: fn _ -> 4 end], module: :rand)
-    result = roll("d6", stub_rand)
-    assert result == 4
+    {:ok, [stub_rand: stub_rand]}
   end
 
-  test "rolling 1d6 returns the randomly rolled value" do
-    stub_rand = Stubr.stub!([uniform: fn _ -> 3 end], module: :rand)
-    result = roll("1d6", stub_rand)
-    assert result == 3
+  test "rolling 'd' throws an argument error", %{stub_rand: stub_rand} do
+    assert_raise(ArgumentError, fn -> roll("d", stub_rand) end)
   end
 
-  test "rolling 2d6 returns two randomly rolled values added together" do
-    stub_rand = Stubr.stub!([uniform: fn _ -> 5 end], module: :rand)
-    result = roll("2d6", stub_rand)
-    assert result == 10
+  test "rolling 'd4' returns the randomly rolled value", %{stub_rand: stub_rand} do
+    assert roll("d4", stub_rand) == 4
   end
 
-  test "rolling 3d6 returns three randomly rolled values added together" do
-    stub_rand = Stubr.stub!([uniform: fn _ -> 5 end], module: :rand)
-    result = roll("3d6", stub_rand)
-    assert result == 15
+  test "rolling '1d' throws an argument error", %{stub_rand: stub_rand} do
+    assert_raise(ArgumentError, fn -> roll("1d", stub_rand) end)
+  end
+
+  test "rolling '1d4' returns the randomly rolled value", %{stub_rand: stub_rand} do
+    assert roll("1d4", stub_rand) == 4
+  end
+
+  test "rolling '2d4' returns two randomly rolled values added together", %{stub_rand: stub_rand} do
+    assert roll("2d4", stub_rand) == 8
+  end
+
+  test "rolling '30d4' returns thirty randomly rolled values added together", %{stub_rand: stub_rand} do
+    assert roll("30d4", stub_rand) == 120
+  end
+
+  test "rolling '1d4+' returns the randomly rolled value plus 0", %{stub_rand: stub_rand} do
+    assert roll("1d4+", stub_rand) == 4
+  end
+
+  test "rolling '1d4+2' returns the randomly rolled value plus 2", %{stub_rand: stub_rand} do
+    assert roll("1d4+2", stub_rand) == 6
+  end
+
+  test "rolling '1d4+200' returns the randomly rolled value plus 200", %{stub_rand: stub_rand} do
+    assert roll("1d4+200", stub_rand) == 204
+  end
+
+  test "rolling '1d4-' returns the randomly rolled value minus 0", %{stub_rand: stub_rand} do
+    assert roll("1d4-", stub_rand) == 4
+  end
+
+  test "rolling '1d4-2' returns the randomly rolled value minus 2", %{stub_rand: stub_rand} do
+    assert roll("1d4-2", stub_rand) == 2
+  end
+
+  test "rolling '1d4-200' returns the randomly rolled value minus 200", %{stub_rand: stub_rand} do
+    assert roll("1d4-200", stub_rand) == -196
+  end
+
+  test "always rolls an integer between 1 and the given die's maximum" do
+    for _ <- 1..10_000 do
+      d4 = roll("1d4")
+
+      assert is_integer(d4)
+      assert d4 >= 1
+      assert d4 <= 4
+    end
+
+    for _ <- 1..10_000 do
+      d6 = roll("1d6")
+
+      assert is_integer(d6)
+      assert d6 >= 1
+      assert d6 <= 6
+    end
+
+    for _ <- 1..10_000 do
+      d8 = roll("1d8")
+
+      assert is_integer(d8)
+      assert d8 >= 1
+      assert d8 <= 8
+    end
+
+    for _ <- 1..10_000 do
+      d10 = roll("1d10")
+
+      assert is_integer(d10)
+      assert d10 >= 1
+      assert d10 <= 10
+    end
+
+    for _ <- 1..10_000 do
+      d12 = roll("1d12")
+
+      assert is_integer(d12)
+      assert d12 >= 1
+      assert d12 <= 12
+    end
+
+    for _ <- 1..10_000 do
+      d20 = roll("1d20")
+
+      assert is_integer(d20)
+      assert d20 >= 1
+      assert d20 <= 20
+    end
+
+    for _ <- 1..10_000 do
+      d100 = roll("1d100")
+
+      assert is_integer(d100)
+      assert d100 >= 1
+      assert d100 <= 100
+    end
+  end
+
+  test "rolling nonsense raises an argument error", %{stub_rand: stub_rand} do
+    assert_raise(ArgumentError, fn -> roll("foo", stub_rand) end)
+    assert_raise(ArgumentError, fn -> roll("12+1d4", stub_rand) end)
+    assert_raise(ArgumentError, fn -> roll("12ad4m+", stub_rand) end)
+    assert_raise(ArgumentError, fn -> roll("1d4+n", stub_rand) end)
   end
 end
